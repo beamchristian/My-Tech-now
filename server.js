56aa56837184b23c99451894e36fcb24d226f18a
beamchristian <beamchristian@yahoo.com>
2021-11-26 09:11:24 -0500

    login, logout

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const { sequelize } = require('./models/Comment');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 // import sequelize connection
 
@@ -14,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
+app.use(session(sess));
 app.use(require('./controllers/'));
 
 // sync sequelize models to the database, then turn on the server
